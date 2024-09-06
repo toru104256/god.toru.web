ece4ef9c25bcaa65dfd3aba8f20bf6fb11f2dea8
toru104256 <kd1401960@st.kobedenshi.ac.jp>
2024-09-06 11:24:15 +0900

    first commit

---
+++ sample1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="title" contect="WWEB開発3回目" />
+    <head>
+    <body>
+        前川徹
+    <body>
+<html>
+
