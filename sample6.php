7a613af7b4c8cdf0577ff6f4127b7a680eb7ba16
toru104256 <kd1401960@st.kobedenshi.ac.jp>
2024-09-10 15:38:50 +0900

    Add files via upload

---
+++ sample6.php
@@ -0,0 +1,16 @@
+<!--html DOCTYP宣言-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>WEB開発2回目</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <meta charset="utf-8">
+
+    <body>
+        <?php print("上月康太郎");?>
+        
+    </body>
+    
+</html>
