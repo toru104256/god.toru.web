599a67792f1307f8f50f3bceceb558eb256a1255
toru104256 <kd1401960@st.kobedenshi.ac.jp>
2024-09-10 15:31:59 +0900

    Add files via upload

---
+++ sample3.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+<html lang="ja">
+<head>
+        <meta charset="utf-8">
+        <title>WEB開発4回目</title>
+    </head>
+    <body>
+        <div>
+            <?php
+                print('現在は'.date('y年 m月 d日 H時 i分 s秒').'です');
+            ?>
+        </div>
+    </body>
