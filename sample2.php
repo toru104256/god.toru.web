364b42a71f7fe7510b81072cb027c725d1f5ca49
toru104256 <kd1401960@st.kobedenshi.ac.jp>
2024-09-10 15:27:46 +0900

    Add files via upload

---
+++ sample2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="title" content="web開発4回目"/>
+    </head>
+    <body>
+    <?php
+        print('24*60*60<br>');
+        print('1日=');
+        print(24*60*60);
+        print('秒');
+    ?>
+    </body>
+</html>
