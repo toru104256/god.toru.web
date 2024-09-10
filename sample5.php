ed442e73b7d43ce195088923dad7d57507f44007
toru104256 <kd1401960@st.kobedenshi.ac.jp>
2024-09-10 15:36:23 +0900

    Add files via upload

---
+++ sample5.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="title" content="web開発4回目"/>
+    </head>
+    <body>
+    <?php
+        for ($i=100; $i>=1; $i-=2){
+            print($i. "<br>");
+        }
+    ?>
+    </body>
+</html>
