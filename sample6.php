32cb0904020d4e5bd03b638e750b462742ca0e5a
toru104256 <kd1401960@st.kobedenshi.ac.jp>
2024-09-11 11:20:10 +0900

    sample6.php

--- sample6.php
+++ sample6.php
@@ -1,16 +1,1 @@
-<!--html DOCTYP宣言-->
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>WEB開発2回目</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <meta charset="utf-8">
 
-    <body>
-        <?php print("abc");?>
-        
-    </body>
-    
-</html>
